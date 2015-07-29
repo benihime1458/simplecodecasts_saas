70478907858a027978c4222ec7ef49e91608e812
benihime1458 <benihime1458@gmail.com>
2015-07-29 21:12:14 +0000

    Added contact page view, action, route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/about' => 'pages#about'
-  get '/about' => 'pages#contact'
+  get '/contact' => 'pages#contact'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
